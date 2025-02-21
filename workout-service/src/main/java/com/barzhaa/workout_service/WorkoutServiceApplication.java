d02d65fc0a64a2b337811ee6b84f595af470a5da
sharimardinov <sharimardinov@icloud.com>
2025-02-21 23:44:29 +0300

    first commit

---
+++ workout-service/src/main/java/com/barzhaa/workout_service/WorkoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.barzhaa.workout_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutServiceApplication.class, args);
+	}
+
+}
